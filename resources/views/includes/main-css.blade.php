69dcbd7cb116e4ec849c7724560e2d8f51359fae
Arden28 <laudbouetoumoussa@gmail.com>
2023-10-07 11:54:47 +0200

    New update 1

--- resources/views/includes/main-css.blade.php
+++ resources/views/includes/main-css.blade.php
@@ -1,15 +1,15 @@
 
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/feather/style.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/flaticon/font/flaticon.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/css/aos.css?v=').time()}}">
-<link rel="stylesheet" href="{{ asset('assets/css/style.css?v=').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/feather/style.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/flaticon/font/flaticon.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/css/aos.css?v=12345678').time()}}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css?v=12345678').time()}}">
 <!-- Bootstrap Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
