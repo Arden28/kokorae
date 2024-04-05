4ae5b4c026f5fc3494570ad6184588e404df5214
Arden28 <laudbouetoumoussa@gmail.com>
2024-04-05 21:29:50 +0200

    Ajout de la réservation

--- resources/views/includes/main-css.blade.php
+++ resources/views/includes/main-css.blade.php
@@ -6,15 +6,15 @@
 <!-- Caveat Font -->
 <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/feather/style.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/flaticon/font/flaticon.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/css/aos.css?v=12345675')}}">
-<link rel="stylesheet" href="{{ asset('assets/css/style.css?12345625')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/feather/style.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/flaticon/font/flaticon.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/aos.css?v=12345622')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/style.css?12345612')}}">
 <!-- Bootstrap Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
