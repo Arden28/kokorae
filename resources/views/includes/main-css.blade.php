1b97da7ec5835da8466545d8a3b181faf887539d
Arden28 <laudbouetoumoussa@gmail.com>
2024-04-06 14:11:17 +0200

    Problème du CSS identifié

--- resources/views/includes/main-css.blade.php
+++ resources/views/includes/main-css.blade.php
@@ -6,14 +6,14 @@
 <!-- Caveat Font -->
 <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css?'.time())}}">
-<link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css?'.time())}}">
-<link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css?'.time())}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css?'.time())}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/feather/style.css?'.time())}}">
-<link rel="stylesheet" href="{{ asset('assets/fonts/flaticon/font/flaticon.css?'.time())}}">
-<link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css?'.time())}}">
-<link rel="stylesheet" href="{{ asset('assets/css/aos.css?'.time())}}">
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css?'.time() )}}">
+<link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css?'.time() )}}">
+<link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css?'.time() )}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css?'.time() )}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/feather/style.css?'.time() )}}">
+<link rel="stylesheet" href="{{ asset('assets/fonts/flaticon/font/flaticon.css?'.time() )}}">
+<link rel="stylesheet" href="{{ asset('assets/css/jquery.fancybox.min.css?'.time() )}}">
+<link rel="stylesheet" href="{{ asset('assets/css/aos.css?'.time() )}}">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css?'.time())}}">
 <!-- Bootstrap Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
