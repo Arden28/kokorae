c4ee9b6ecaa9864fcc4b331477c5ce19cc731211
Arden28 <laudbouetoumoussa@gmail.com>
2024-04-20 15:31:10 +0200

    Update the website login page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('parent_kover_id')->nullable();
             $table->string('name');
             $table->string('society')->nullable();
-            $table->string('phone')->unique();
+            $table->string('phone')->unique()->nullable();
             $table->timestamp('phone_verified_at')->nullable();
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
